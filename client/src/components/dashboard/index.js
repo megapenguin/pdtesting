806f05faed4fa7c4149745cd77779e2d9518e306
megapenguin <megapenguin66@gmail.com>
2021-03-10 21:14:36 +0800

    03/10/2021

--- client/src/components/dashboard/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StudentChartShit = React.lazy(() => import("./StudentChartShit"));
-
-//import StudentChartShit from "./StudentChartShit";
-
-export { StudentChartShit };
